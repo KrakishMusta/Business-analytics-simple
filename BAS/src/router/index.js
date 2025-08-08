5800244b33dae8d2eedd3b86562ecda3eb9ef1c6
KrakishMusta <project.zero.one01@gmail.com>
2025-08-08 17:31:14 +0700

    Изменение настроек развёртывания

--- BAS/src/router/index.js
+++ BAS/src/router/index.js
@@ -44,7 +44,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/Business-analytics-simple/'),
   routes
 })
 
